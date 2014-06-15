2c43ff9f397f9e6f327d4705ab3de86874204899
Oleg Shparber <trollixx@gmail.com>
2014-06-15 00:56:56 -0700

    Sophisticated shortcut editor
    Based on Qt Creator's shortcut editor.

--- src/preferencesdialog.cpp
+++ src/preferencesdialog.cpp
@@ -7,6 +7,7 @@
 
 #include <QFileDialog>
 #include <QFontDialog>
+#include <QKeyEvent>
 #include <QStyleFactory>
 
 PreferencesDialog::PreferencesDialog(QWidget *parent) :
@@ -90,8 +91,15 @@ PreferencesDialog::PreferencesDialog(QWidget *parent) :
     dropWidthSpinBox->setValue(preferences->dropWidht);
     dropShortCutEdit->setText(preferences->dropShortCut.toString());
 
+    /// Shortcuts Page
+    m_keyNum = m_key[0] = m_key[1] = m_key[2] = m_key[3] = 0;
 
     actionTreeWidget->sortByColumn(0, Qt::AscendingOrder);
+    shortcutLineEdit->installEventFilter(this);
+
+    connect(actionTreeWidget, &QTreeWidget::currentItemChanged, this, &PreferencesDialog::selectAction);
+    connect(clearShortcutButton, &QPushButton::clicked, this, &PreferencesDialog::clearShortcut);
+    connect(resetShortcutButton, &QPushButton::clicked, this, &PreferencesDialog::resetShortcut);
 
     QFont userEditedShortcutFont;
     userEditedShortcutFont.setBold(true);
@@ -101,12 +109,10 @@ PreferencesDialog::PreferencesDialog(QWidget *parent) :
     foreach (const ActionInfo &actionInfo, ActionManager::registry()) {
         QTreeWidgetItem *item = new QTreeWidgetItem(actionTreeWidget);
         item->setText(0, ActionManager::clearActionText(actionInfo.text));
-        if (actionInfo.shortcut.isEmpty()) {
-            item->setText(1, actionInfo.defaultShortcut.toString());
-        } else {
+        item->setData(0, Qt::UserRole, actionInfo.id);
+        if (actionInfo.shortcut != actionInfo.defaultShortcut)
             item->setFont(1, userEditedShortcutFont);
-            item->setText(1, actionInfo.shortcut.toString());
-        }
+        item->setText(1, actionInfo.shortcut.toString(QKeySequence::NativeText));
     }
 }
 
@@ -145,7 +151,7 @@ void PreferencesDialog::apply()
     preferences->historyLimited = historyLimited->isChecked();
     preferences->historyLimitedTo = historyLimitedTo->value();
 
-    saveShortcuts();
+    applyShortcuts();
 
     preferences->dropShowOnStart = dropShowOnStartCheckBox->isChecked();
     preferences->dropHeight = dropHeightSpinBox->value();
@@ -157,11 +163,122 @@ void PreferencesDialog::apply()
     preferences->emitChanged();
 }
 
-void PreferencesDialog::setFontSample(const QFont &f)
+bool PreferencesDialog::eventFilter(QObject *object, QEvent *event)
 {
-    fontSampleLabel->setFont(f);
-    QString sample("%1 %2 pt");
-    fontSampleLabel->setText(sample.arg(f.family()).arg(f.pointSize()));
+    Q_UNUSED(object)
+
+    if (event->type() == QEvent::KeyPress) {
+        QKeyEvent *k = static_cast<QKeyEvent*>(event);
+        handleKeyEvent(k);
+        return true;
+    }
+
+    if (event->type() == QEvent::Shortcut || event->type() == QEvent::KeyRelease )
+        return true;
+
+    if (event->type() == QEvent::ShortcutOverride) {
+        event->accept();
+        return true;
+    }
+
+    return false;
+}
+
+void PreferencesDialog::selectAction(QTreeWidgetItem *item)
+{
+    shortcutEditGroupBox->setEnabled(true);
+
+    const QString actionId = item->data(0, Qt::UserRole).toString();
+    const QKeySequence ks = ActionManager::actionInfo(actionId).shortcut;
+
+    m_key[0] = m_key[1] = m_key[2] = m_key[3] = 0;
+    m_keyNum = ks.count();
+    for (int i = 0; i < m_keyNum; ++i)
+        m_key[i] = ks[i];
+    updateCurrentShortcut(ks);
+}
+
+void PreferencesDialog::clearShortcut()
+{
+    m_keyNum = m_key[0] = m_key[1] = m_key[2] = m_key[3] = 0;
+    updateCurrentShortcut(QKeySequence());
+}
+
+void PreferencesDialog::resetShortcut()
+{
+    QTreeWidgetItem *item = actionTreeWidget->currentItem();
+    const QString actionId = item->data(0, Qt::UserRole).toString();
+    const QKeySequence ks = ActionManager::actionInfo(actionId).defaultShortcut;
+    updateCurrentShortcut(ks);
+    shortcutLineEdit->setText(ks.toString(QKeySequence::NativeText));
+}
+
+void PreferencesDialog::applyShortcuts()
+{
+    for (int i = 0; i < actionTreeWidget->topLevelItemCount(); ++i) {
+        QTreeWidgetItem *item = actionTreeWidget->topLevelItem(i);
+        const QString actionId = item->data(0, Qt::UserRole).toString();
+        ActionManager::updateShortcut(actionId, QKeySequence(item->text(1)));
+    }
+}
+
+void PreferencesDialog::handleKeyEvent(QKeyEvent *event)
+{
+    int nextKey = event->key();
+    if (m_keyNum > 3 ||
+            nextKey == Qt::Key_Control ||
+            nextKey == Qt::Key_Shift ||
+            nextKey == Qt::Key_Meta ||
+            nextKey == Qt::Key_Alt)
+        return;
+
+    nextKey |= translateModifiers(event->modifiers(), event->text());
+    switch (m_keyNum) {
+    case 0:
+        m_key[0] = nextKey;
+        break;
+    case 1:
+        m_key[1] = nextKey;
+        break;
+    case 2:
+        m_key[2] = nextKey;
+        break;
+    case 3:
+        m_key[3] = nextKey;
+        break;
+    default:
+        break;
+    }
+    ++m_keyNum;
+    QKeySequence ks(m_key[0], m_key[1], m_key[2], m_key[3]);
+    updateCurrentShortcut(ks);
+    event->accept();
+}
+
+int PreferencesDialog::translateModifiers(Qt::KeyboardModifiers state, const QString &text)
+{
+    int result = 0;
+    // The shift modifier only counts when it is not used to type a symbol
+    // that is only reachable using the shift key anyway
+    if ((state & Qt::ShiftModifier) && (text.size() == 0
+                                        || !text.at(0).isPrint()
+                                        || text.at(0).isLetterOrNumber()
+                                        || text.at(0).isSpace()))
+        result |= Qt::SHIFT;
+    if (state & Qt::ControlModifier)
+        result |= Qt::CTRL;
+    if (state & Qt::MetaModifier)
+        result |= Qt::META;
+    if (state & Qt::AltModifier)
+        result |= Qt::ALT;
+    return result;
+}
+
+void PreferencesDialog::updateCurrentShortcut(const QKeySequence &ks)
+{
+    shortcutLineEdit->setText(ks.toString(QKeySequence::NativeText));
+    QTreeWidgetItem *item = actionTreeWidget->currentItem();
+    item->setText(1, ks.toString(QKeySequence::NativeText));
 }
 
 void PreferencesDialog::changeFontButton_clicked()
@@ -174,7 +291,9 @@ void PreferencesDialog::changeFontButton_clicked()
     setFontSample(font);
 }
 
-void PreferencesDialog::saveShortcuts()
+void PreferencesDialog::setFontSample(const QFont &f)
 {
-    /// TODO: ActionManager support
+    fontSampleLabel->setFont(f);
+    QString sample("%1 %2 pt");
+    fontSampleLabel->setText(sample.arg(f.family()).arg(f.pointSize()));
 }
--- src/preferencesdialog.h
+++ src/preferencesdialog.h
@@ -3,21 +3,37 @@
 
 #include "ui_preferencesdialog.h"
 
+class QKeyEvent;
+class QTreeWidgetItem;
+
 class PreferencesDialog : public QDialog, public Ui::PreferencesDialog
 {
     Q_OBJECT
 public:
     explicit PreferencesDialog(QWidget *parent = nullptr);
 
-private slots:
-    void saveShortcuts();
-    void apply();
+protected:
     void accept() override;
+    bool eventFilter(QObject *object, QEvent *event) override;
 
-    void changeFontButton_clicked();
+private slots:
+    void selectAction(QTreeWidgetItem *item);
+    void clearShortcut();
+    void resetShortcut();
 
 private:
+    void apply();
+    void applyShortcuts();
+
+    void handleKeyEvent(QKeyEvent *event);
+    int translateModifiers(Qt::KeyboardModifiers state, const QString &text);
+    void updateCurrentShortcut(const QKeySequence &ks);
+
+    void changeFontButton_clicked();
     void setFontSample(const QFont &f);
+
+    int m_key[4];
+    int m_keyNum;
 };
 
 #endif // PREFERENCESDIALOG_H
