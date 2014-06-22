a415df563a50041560ccd24c6cd046ca9161cf71
Oleg Shparber <trollixx@gmail.com>
2014-06-22 00:57:13 -0700

    Fix broken signal-slot connections in PreferencesDialog

--- src/preferencesdialog.h
+++ src/preferencesdialog.h
@@ -19,19 +19,20 @@ protected:
     bool eventFilter(QObject *object, QEvent *event) override;
 
 private slots:
+    void apply();
+    void changeFontButton_clicked();
+
     void selectAction(QTreeWidgetItem *item);
     void clearShortcut();
     void resetShortcut();
 
 private:
-    void apply();
     void applyShortcuts();
 
     void handleKeyEvent(QKeyEvent *event);
     int translateModifiers(Qt::KeyboardModifiers state, const QString &text);
     void updateCurrentShortcut(const QKeySequence &ks);
 
-    void changeFontButton_clicked();
     void setFontSample(const QFont &f);
 
     Preferences * const m_preferences = nullptr;
